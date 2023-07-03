e7dccf3a5f6399a2e099f2c7b828ff6c1a143d1d
Alejandra Ugalde <alghey@gmail.com>
2023-07-03 17:25:54 -0600

    Lectura de datos del potenciómetro en el Tx
    Se agregó la lectura de los datos obtenidos del potenciómetro en el código del Transmisior

--- Envio_Datos/envio_4datos/envio_4datos.ino
+++ Envio_Datos/envio_4datos/envio_4datos.ino
@@ -2,10 +2,12 @@
 //#include <nRF24L01.h> //Librería de NRF24
 #include <RF24.h> //lIBRERIA DE nrf24
 
+#define POT_PIN A0
+
 RF24 radio(9, 10); // Configura los pines CE y CSN
 
 const byte direccion_receptor[6] = "00001"; // Dirección del receptor
-
+long potencia;
 //Estructura de datos: 4 enteros
 struct Datos {
   int dato1;
@@ -21,15 +23,18 @@ void setup() {
 }
 
 void loop() {
+
+  potencia = analogRead(POT_PIN);
+
   Datos datos;
-  datos.dato1 = 2711;
+  datos.dato1 = int(potencia);
   datos.dato2 = 2806;
   datos.dato3 = 2106;
   datos.dato4 = 609;
 
   // Envía los datos
   radio.write(&datos, sizeof(datos));
-
+  Serial.println(datos.dato1);
   Serial.println("Datos enviados");
 
   delay(1000); // Espera un segundo antes de enviar los datos nuevamente
