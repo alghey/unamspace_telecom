8c8d03cc78964da427c3b8376c2ca60104110e63
Eder-Berno <ederac1@hotmail.com>
2023-07-03 16:38:41 -0600

    Update Joystick_Coords.ino
    Se implementó las funciones del puente  H

--- Joystick_Coords/Joystick_Coords.ino
+++ Joystick_Coords/Joystick_Coords.ino
@@ -4,9 +4,24 @@ const int SW = 2; // Pin digital del SW
 int valorX = 0; // Variable para almacenar el valor de X
 int valorY = 0; // Variable para almacenar el valor de Y
 bool boton = false; // Variable para validar el estado del botón
+//Variables para el puente H:
+//variables Motores
+int MotorIzquierdoAdelante  = 3; //pin Digital
+int MotorIzquierdoAtras     = 4; //pin Digital
+int MotorDerechoAdelante    = 7; //pin Digital
+int MotorDerechoAtras       = 8; //pin Digital
+int pinVelocidadDerecha     = 6; //pin PWM
+int pinVelocidadIzquierda   = 5; //pin PWM
+int velocidad = 125;//velocidad motores(0-255)
 
 void setup() {
   Serial.begin(9600); // Inicia la comunicación serial a 9600 baudios
+   //activar señales del puente h
+  pinMode(MotorIzquierdoAdelante,OUTPUT);
+  pinMode(MotorDerechoAdelante,OUTPUT);
+  pinMode(MotorIzquierdoAtras,OUTPUT);
+  pinMode(MotorDerechoAtras,OUTPUT);
+  Serial.println("Esta es una prueba para el control del puente H");
 }
 
 void loop() {
@@ -33,3 +48,60 @@ void loop() {
   
   
 }
+
+//FUNCIONES
+void carroAvanzaAdelante(){
+    Serial.println("avanzando hacia adelante");
+    digitalWrite(MotorDerechoAdelante,HIGH);
+    digitalWrite(MotorIzquierdoAdelante,HIGH);
+    digitalWrite(MotorDerechoAtras,LOW);
+    digitalWrite(MotorIzquierdoAtras,LOW);
+    delay(1000);
+  }
+  
+void carroAvanzaAtras(){
+    Serial.println("avanzando hacia atras");
+    digitalWrite(MotorDerechoAdelante,LOW);
+    digitalWrite(MotorIzquierdoAdelante,LOW);
+    digitalWrite(MotorDerechoAtras,HIGH);
+    digitalWrite(MotorIzquierdoAtras,HIGH);
+    delay(1000);
+  }
+  
+void carroGiraDerecha(){
+    Serial.println("girando a la derecha");
+    digitalWrite(MotorDerechoAdelante,LOW);
+    digitalWrite(MotorIzquierdoAdelante,HIGH);
+    digitalWrite(MotorDerechoAtras,HIGH);
+    digitalWrite(MotorIzquierdoAtras,LOW);
+    digitalWrite(13,HIGH);
+  }
+  
+void carroGiraIzquierda(){
+    Serial.println("girando a la izquierda");
+    digitalWrite(MotorDerechoAdelante,HIGH);
+    digitalWrite(MotorIzquierdoAdelante,LOW);
+    digitalWrite(MotorDerechoAtras,LOW);
+    digitalWrite(MotorIzquierdoAtras,HIGH);
+    digitalWrite(13,LOW);
+  }
+  
+void carroDetenido(){
+    Serial.println("automovil detenido");
+    digitalWrite(MotorDerechoAdelante,LOW);
+    digitalWrite(MotorIzquierdoAdelante,LOW);
+    digitalWrite(MotorDerechoAtras,LOW);
+    digitalWrite(MotorIzquierdoAtras,LOW);
+  }
+void pruebaMotores(){
+    Serial.println("automovil detenido");
+    digitalWrite(MotorDerechoAdelante,HIGH);
+    digitalWrite(MotorIzquierdoAdelante,LOW);
+    digitalWrite(MotorDerechoAtras,HIGH);
+    digitalWrite(MotorIzquierdoAtras,LOW);
+    delay(200);
+    velocidad = velocidad+1;
+    if (velocidad == 250){
+      velocidad = 0;
+      }
+  }
