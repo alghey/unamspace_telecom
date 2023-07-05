ddee5bd4e2c16117c657664dba6734914e303c83
joshUnknown2806 <38014635+joshUnknown2806@users.noreply.github.com>
2023-07-05 16:31:30 -0600

    LORA envío de 7 datos

--- LORA_T/LORA_T.ino
+++ LORA_T/LORA_T.ino
@@ -3,26 +3,42 @@
 
 LoRa_E220 e220ttl(2, 3); // Arduino RX --> e220 TX - Arduino TX --> e220 RX
 
+struct telemetria {
+  float dato1;
+  float dato2;
+  float dato3;
+  float dato4;
+  float dato5;
+  float dato6;
+  float dato7;
+};
+
+
 void setup() {
   Serial.begin(9600);
   delay(500);
 
-  Serial.println("(W) Adelante  (S) Atrás   (A) Izquierda  (D) Derecha  (E) Detener");
+  Serial.println("Prueba");
   
   // Startup all pins and UART
   e220ttl.begin();
-  ResponseStatus rs = e220ttl.sendMessage("Hello, world?");
   // Check If there is some problem of successfully send
-  Serial.println(rs.getResponseDescription());
 }
-
+int i = 0;
 void loop() {
-  if (Serial.available()) {
-    String input = Serial.readString();
+  telemetria tele;
+  tele.dato1 = 95.12;
+  tele.dato2 = 0.25;
+  tele.dato3 = -6.25;
+  tele.dato4 = 3.25;
+  tele.dato5 = -0.25;
+  tele.dato6 = 25.36;
+  tele.dato7 = 25.365;
 
-    ResponseStatus rs = e220ttl.sendMessage(input);
-    Serial.println(input);
-
-    delay(100); // Agregar un pequeño retraso después de enviar el mensaje
-  }
+  //if (Serial.available()) {
+  Serial.println(i);
+  i++;
+  ResponseStatus rs = e220ttl.sendMessage(&tele, sizeof(telemetria));
+  Serial.println(rs.getResponseDescription()); // Agregar un pequeño retraso después de enviar el mensaje
+  //}
 }
